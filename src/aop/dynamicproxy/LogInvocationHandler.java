3e871b64c0bab027ee7ca4b0b8af669e0d14cbac
l0623 <lifj@dtdream.com>
2019-02-15 18:08:01 +0800

    实现动态代理的两种方法（基于JDK的代理和基于CGLIB的代理）

---
+++ src/aop/dynamicproxy/LogInvocationHandler.java
@@ -0,0 +1,42 @@
+package aop.dynamicproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
+/**
+ * 该方法是基于jdk实现的动态代理
+ */
+interface Hello {
+    String sayHello(String str);
+}
+class HelloImp implements Hello{
+    @Override
+    public String sayHello(String str) {
+        return "HelloImp: " + str;
+    }
+}
+public class LogInvocationHandler implements InvocationHandler {
+    private Hello hello;
+
+    public LogInvocationHandler(Hello hello) {
+        this.hello = hello;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if("sayHello".equals(method.getName())) {
+            System.out.println("You said: " + Arrays.toString(args));
+        }
+        return method.invoke(hello, args);
+    }
+
+    public static void main(String[] args) {
+        Hello hello = (Hello) Proxy.newProxyInstance(
+                ClassLoader.getSystemClassLoader(), // 1. 类加载器
+                new Class<?>[] {Hello.class}, // 2. 代理需要实现的接口，可以有多个
+                new LogInvocationHandler(new HelloImp()));// 3. 方法调用的实际处理者
+        System.out.println(hello.sayHello("I love you!"));
+    }
+}
---
+++ src/aop/dynamicproxy/MyMethodInterceptor.java
@@ -0,0 +1,35 @@
+package aop.dynamicproxy;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+/**
+ * 该类是使用CGLIB实现的动态代理
+ */
+class HelloConcrete {
+    public String sayHello(String str) {
+        return "HelloConcrete: " + str;
+    }
+}
+
+public class MyMethodInterceptor implements MethodInterceptor {
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        System.out.println("You said: " + Arrays.toString(args));
+        return proxy.invokeSuper(obj, args);
+    }
+
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(HelloConcrete.class);
+        enhancer.setCallback(new MyMethodInterceptor());
+
+        HelloConcrete hello = (HelloConcrete)enhancer.create();
+        System.out.println(hello.sayHello("I love you!"));
+    }
+}
